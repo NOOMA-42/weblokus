db7865a1608a83b6e580c2cd5e8e71a317d1ed3d
Arthurddd <a0978222892@gmail.com>
2019-06-24 20:56:54 +0800

    first commit without dnd

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom" ;
+import WebLokus from './components/WebLokus' ;
+import 'bootstrap/dist/css/bootstrap.min.css' ;
+import "index.scss";
+
+ReactDOM.render(  <WebLokus/>,  document.getElementById("root"));
