493b8fe3a2cb77bb0d282d05b83890eee9ca7f78
Arthurddd <a0978222892@gmail.com>
2019-06-24 21:58:10 +0800

    misplaced items removal

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom" ;
-import WebLokus from './components/WebLokus' ;
-import 'bootstrap/dist/css/bootstrap.min.css' ;
-import "index.scss";
-
-ReactDOM.render(  <WebLokus/>,  document.getElementById("root"));
