7b47dd83142cc72af88beb1e2d2fc78c92de2739
zihching chen <virginia.chen2007@gmail.com>
2019-07-01 21:09:31 +0800

    backend post and get ranking

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,16 +7,20 @@ import regeneratorRuntime from "regenerator-runtime";
 import { monitor } from '@colyseus/monitor';
 //const socialRoutes = require("@colyseus/social/express").default;
 import playGround from './Room/PlayGround';
-
+const bodyParser = require("body-parser")
 const port = process.env.PORT || 2567;
 const app = express();
 app.use(cors());
+
 const server = http.createServer(app);
 const gameServer = new Server({ server });
 
-const Rank = require("../models/Rank")
+let Rank = require("../models/Rank")
 const mongoose = require("mongoose")
 const playboradRoutes = express.Router()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}));
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/ranking', { useNewUrlParser: true });
@@ -24,44 +28,33 @@ const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
 })
-app.use("/ranks",playboradRoutes)
-/*
-app.post('/playborad', (req, res) => {
-    console.log('Inside the homepage callback')
-    res.send(`Helle\n`)
-    console.log(req.body)
-    let rank = new Rank(req.body);
-    rank.save()
-        .then(post=>{
-            res.send("in the function")
-            res.status(200).json({'rank':'rank add successfully'})
-            })
-        .catch(err=>
-            res.status(400).send("rank error"))
-  })*/
-  /*
-app.post('/playborad',(req,res)=>{
-    console.log("posttt")
-    res.send("post")
-})*/
-/*
-playboradRoutes.get('/',function(req,res,next){
-    res.send("respond with rescorce")
-})*/
-playboradRoutes.route('/playborad').get(function(req,res){
-    res.send("route get")
+//
+
+playboradRoutes.route('/').get(function(req,res){
+    Rank.find(function(err,ranks){
+        if(err){
+            console.log("errrrror",err)
+        }
+        else{
+            res.json(ranks)
+        }
+    })
+ 
 })
+
 
 playboradRoutes.route('/playborad').post(function(req, res) {
     let rank = new Rank(req.body);
     rank.save()
         .then(post => {
+            console.log("hi",req.body)
             res.status(200).json({'rank': 'rank added successfully'});
         })
         .catch(err => {
             res.status(400).send('adding new rank failed');
         });
 });
+app.use("/ranks",playboradRoutes)
 // register your room handlers
 /*
 There could be many rooms in a single registrant
